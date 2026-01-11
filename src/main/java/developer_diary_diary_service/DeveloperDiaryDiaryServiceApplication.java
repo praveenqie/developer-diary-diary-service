1f68e2adcec39478ee3063d52bd1bb75de1085df
praveenqie <praveenkumarks11@gmail.com>
2026-01-11 17:46:38 +0530

    initial commit for the dairy service

---
+++ src/main/java/developer_diary_diary_service/DeveloperDiaryDiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package developer_diary_diary_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperDiaryDiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperDiaryDiaryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/developer_diary_diary_service/DeveloperDiaryDiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package developer_diary_diary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperDiaryDiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
